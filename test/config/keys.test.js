a87221e5861db7bf2506873e139bd5dda2b52e66
missmahi55 <mahakagarwal248@gmail.com>
2021-09-21 10:13:55 +0530

    update

--- test/config/keys.test.js
+++ test/config/keys.test.js
@@ -3,6 +3,6 @@ const config = require("../../config/keys");
 
 describe("Test config file", ()=>{
     it("should return an object of the configurations", async ()=>{
-        assert.Equal("object", typeof config);
+        assert.strictEqual("object", typeof config);
     });
 });
